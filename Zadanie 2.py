ab05557262ada834596b432cdc8aa9457376a030
Your Name here only <greg152295@gmail.com>
2019-11-16 16:59:06 +0200

    Sort and sorted

--- Zadanie 2.py
+++ Zadanie 2.py
@@ -1,4 +1,9 @@
 list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
 print(list_to_sort)
+lista = sorted(list_to_sort, key=lambda x:(x[1],x[2]))
+print(lista)
+
+list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
+print(list_to_sort)
 list_to_sort.sort(key=lambda x:(x[1],x[2]))
 print(list_to_sort)
